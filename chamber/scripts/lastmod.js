64189947915edff7b95ef46388fd00c8218f259d
yesicagallucci <yesica.gallucci@gmail.com>
2024-02-03 22:54:38 -0300

    chamber site

---
+++ chamber/scripts/lastmod.js
@@ -0,0 +1,4 @@
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last modification: " + lastModified;
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
