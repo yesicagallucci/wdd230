303cb8c40d83cf9d33d81a37d3827a4b598613ff
yesicagallucci <yesica.gallucci@gmail.com>
2024-02-21 11:25:10 -0300

    visits

---
+++ scripts/numvisits.js
@@ -0,0 +1,13 @@
+
+const visitDisplay = document.querySelector("#visits");
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !== 0){
+   visitsDisplay.textContent = numVisits;
+} else{
+   visitsDisplay.textContent = 'This is your first visit';
+ 
+}
+
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
