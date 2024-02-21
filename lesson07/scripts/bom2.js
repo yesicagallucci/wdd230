ac74d0b78bcf6b80affd2e036b5dd27fc1b22ab7
yesicagallucci <yesica.gallucci@gmail.com>
2024-02-21 16:46:15 -0300

    more changes to bom2

--- lesson07/scripts/bom2.js
+++ lesson07/scripts/bom2.js
@@ -3,12 +3,14 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 let chaptersArray = getChapterList() || [];
-chaptersArray.array.forEach(chapter => {
+
+chaptersArray.forEach(chapter => {
     displayList (chapter);
 });
 
 button.addEventListener('click', () => {
-	if (input.value !=''){
+	if (input.value !='') {
+
         displayList(input.value);
         chaptersArray.push(input.value);
         setChapterList();
@@ -16,17 +18,19 @@ button.addEventListener('click', () => {
         input.focus();
     }
 		
-	});
+});
     function displayList(item) {
-        let li = document.createElementNS('li');
+        
+        let li = document.createElement('li');
         let deleteButton = document.createElement('button');
-		li.textContent = item;
+		
+        li.textContent = item;
 		deleteButton.textContent = 'X';
         deleteButton.classList.add('delete');
 		li.append(deleteButton);
 		list.append(li);
 
-		deleteButton.addEventListener('click', function (){
+		deleteButton.addEventListener('click', () => {
 			list.removeChild(li);
             deleteChapter(li.textContent);
 			input.focus();
@@ -41,24 +45,10 @@ button.addEventListener('click', () => {
         return JSON.parse(localStorage.getItem('myFavBOMList'));
     }
 	
-    function deleteChapter(chapter) {
-        chapter = chapte.slice(0, chapter.length -1);
+    function deleteChapter(chapter){
+        chapter = chapter.slice(0, chapter.length -1);
         chaptersArray = chaptersArray.filter((item) => item !== chapter);
         setChapterList();
     }
     
 
-
-   /*const li = document.createElement('li');
-		const deleteButton = document.createElement('button');
-		li.textContent = input.value;
-		deleteButton.textContent = 'X';
-		li.append(deleteButton);
-		list.append(li);
-		deleteButton.addEventListener('click', function (){
-			list.removeChild(li);
-			input.focus();
-		});
-		input.focus();
-		input.value = '';
-		}*/
