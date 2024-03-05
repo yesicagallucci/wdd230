d9715378ddba61945b905f647f7dae5bebe2837d
yesicagallucci <yesica.gallucci@gmail.com>
2024-03-05 20:27:06 -0300

    lesson 9 homework

---
+++ lesson09/heroes-jsonexample/scripts/hero.js
@@ -0,0 +1,106 @@
+//Top-level function
+async function populate() {
+    const requestURL =
+      "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+    const request = new Request(requestURL);
+  
+    const response = await fetch(request);
+    const superHeroes = await response.json();
+  
+    populateHeader(superHeroes);
+    populateHeroes(superHeroes);
+  }
+
+  //Populating the header
+  function populateHeader(obj) {
+    const header = document.querySelector("header");
+    const myH1 = document.createElement("h1");
+    myH1.textContent = obj.squadName;
+    header.appendChild(myH1);
+  
+    const myPara = document.createElement("p");
+    myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+    header.appendChild(myPara);
+  }
+  
+  //Creating the hero information cards
+  function populateHeroes(obj) {
+    const section = document.querySelector("section");
+    const heroes = obj.members;
+  
+    for (const hero of heroes) {
+      const myArticle = document.createElement("article");
+      const myH2 = document.createElement("h2");
+      const myPara1 = document.createElement("p");
+      const myPara2 = document.createElement("p");
+      const myPara3 = document.createElement("p");
+      const myList = document.createElement("ul");
+  
+      myH2.textContent = hero.name;
+      myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
+      myPara2.textContent = `Age: ${hero.age}`;
+      myPara3.textContent = "Superpowers:";
+  
+      const superPowers = hero.powers;
+      for (const power of superPowers) {
+        const listItem = document.createElement("li");
+        listItem.textContent = power;
+        myList.appendChild(listItem);
+      }
+  
+      myArticle.appendChild(myH2);
+      myArticle.appendChild(myPara1);
+      myArticle.appendChild(myPara2);
+      myArticle.appendChild(myPara3);
+      myArticle.appendChild(myList);
+  
+      section.appendChild(myArticle);
+    }
+  }
+  //Calling the top-level function
+  populate();
+
+  //JSON
+  /*
+   {
+  "squadName" : "Super Hero Squad",
+  "homeTown" : "Metro City",
+  "formed" : 2016,
+  "secretBase" : "Super tower",
+  "active" : true,
+  "members" : [
+    {
+      "name" : "Molecule Man",
+      "age" : 29,
+      "secretIdentity" : "Dan Jukes",
+      "powers" : [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name" : "Madame Uppercut",
+      "age" : 39,
+      "secretIdentity" : "Jane Wilson",
+      "powers" : [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name" : "Eternal Flame",
+      "age" : 1000000,
+      "secretIdentity" : "Unknown",
+      "powers" : [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+  */
---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,50 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cars = document.querySelector('#cards');
+async function getProphetData(){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    //console.table(data.prophets);
+    //Calling a function // note that we reference the prophets array of the JSON data object, not just the object
+    displayProphets(data.prophets);
+}
+
+/*Define a function expression named "displayProphets" that handles a single parameter named "prophets" somewhere in your js file. Use an arrow expression to contain statements that will process the parameter value and build a card for each prophet.
+Inside the function, use a forEach loop with the array parameter to process each "prophet" record one at a time, creating a new card each time.*/
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section')
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let death = document.createElement('p');
+
+        // Build the h2 content out to show the prophet's full name
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; 
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    death.textContent = `Date of Death: ${prophet.death}`;
+    // Build the image portrait by setting all the relevant attributes
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name}${prophet.lastname}`);
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); 
+    card.appendChild(portrait);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(death);
+    
+
+    cards.appendChild(card);
+
+
+    });
+}
+
+getProphetData();
