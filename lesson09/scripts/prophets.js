991237af42e06f2b5be0663f42c800f4fb08550e
yesicagallucci <yesica.gallucci@gmail.com>
2024-03-06 18:30:16 -0300

    learning activities JSON

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,5 +1,5 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-const cars = document.querySelector('#cards');
+const cards = document.querySelector('#cards');
 async function getProphetData(){
     const response = await fetch(url);
     const data = await response.json();
--- scripts/links.js
+++ scripts/links.js
@@ -3,7 +3,26 @@ const linksURL = "https://yesicagallucci.github.io/wdd230/data/links.json";
 async function getLinks(){
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
+    displayLinks(data.lessons);
+}
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        let card = document.createElement('ul');
+        let week = document.createElement('li');
+        let description = document.createElement('a');
+
+        
+        week.textContent = `${lesson.lesson}`;
+        description.textContent = `${lesson.url} ${lesson.title}`;
+        
+        card.appendChild(week);
+        card.appendChild(description);
+
+        linksURL.appendChild(card);
+        
+        
+    });
 }
 
 getLinks();
