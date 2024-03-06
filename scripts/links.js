700601f442eee694e01273f68a2b34b4e446b86a
yesicagallucci <yesica.gallucci@gmail.com>
2024-03-06 17:32:05 -0300

    fixing an error

--- scripts/links.js
+++ scripts/links.js
@@ -1,7 +1,7 @@
 const baseURL = "https://yesicagallucci.github.io/wdd230/";
 const linksURL = "https://yesicagallucci.github.io/wdd230/data/links.json";
 async function getLinks(){
-    const response = await fetch(baseURL);
+    const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
 }
