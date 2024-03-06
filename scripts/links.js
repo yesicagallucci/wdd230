fbe088e9a84c596f57afdb7b877843a9ed5efa0c
yesicagallucci <yesica.gallucci@gmail.com>
2024-03-06 17:28:46 -0300

    console log

--- scripts/links.js
+++ scripts/links.js
@@ -3,6 +3,7 @@ const linksURL = "https://yesicagallucci.github.io/wdd230/data/links.json";
 async function getLinks(){
     const response = await fetch(baseURL);
     const data = await response.json();
+    console.log(data);
 }
 
 getLinks();
