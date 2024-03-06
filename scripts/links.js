a11885052c386a8f9473b39262bcd0406c9cf78a
yesicagallucci <yesica.gallucci@gmail.com>
2024-03-06 18:46:47 -0300

    fixing  json menu

--- scripts/links.js
+++ scripts/links.js
@@ -1,25 +1,27 @@
 const baseURL = "https://yesicagallucci.github.io/wdd230/";
 const linksURL = "https://yesicagallucci.github.io/wdd230/data/links.json";
+const lessons = document.querySelector('#json');
+
 async function getLinks(){
     const response = await fetch(linksURL);
     const data = await response.json();
     //console.log(data);
     displayLinks(data.lessons);
 }
-const displayLinks = (lessons) => {
-    lessons.forEach((lesson) => {
-        let card = document.createElement('ul');
-        let week = document.createElement('li');
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        
+        let lesson = document.createElement('li');
         let description = document.createElement('a');
 
         
         week.textContent = `${lesson.lesson}`;
         description.textContent = `${lesson.url} ${lesson.title}`;
         
-        card.appendChild(week);
-        card.appendChild(description);
+      
+        lesson.appendChild(description);
 
-        linksURL.appendChild(card);
+        lessons.appendChild(lesson);
         
         
     });
