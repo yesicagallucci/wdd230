54800ba2358b8127647763d94297276c5f887acd
yesicagallucci <yesica.gallucci@gmail.com>
2024-03-09 12:21:52 -0300

    json directory file tryout

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,13 +1,14 @@
 const baseURL = "https://yesicagallucci.github.io/wdd230/";
-const linksURL = "https://yesicagallucci.github.io/wdd230/chamber/data/directory.json";
+const dataURL = "https://yesicagallucci.github.io/wdd230/chamber/data/directory.json";
 const members = document.querySelector('#members');
 
-async function getMembers(){
-    const response = await fetch(linksURL);
+async function getMembers() {
+    const response = await fetch(dataURL);
     const data = await response.json();
     console.log(data);
 
-    //displayLinks(data.lessons);
+    //displayMembers(data.lessons);
 }
 
 getMembers();
+
